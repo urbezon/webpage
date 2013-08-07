e1dd3606cad28e5f5d0e2ffc25716086c476daed
urbezon <urbezon@gmail.com>
2013-08-07 19:02:00 +0000

    Web.js updated

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
+var content = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
